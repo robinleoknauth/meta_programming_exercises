1cf760206ca7d82e66c8d0df72d2ea997706ee29
LeoXugu <robinleoknauth@gmail.com>
2017-09-19 10:59:48 -0700

    implement own version of attr_accessor

--- Module_Overrides_ancestors.rb
+++ Module_Overrides_ancestors.rb
@@ -8,6 +8,6 @@ module ModuleOverrides
   end
 end
 
-module Module
+class Module
   prepend ModuleOverrides
 end
---
+++ attr_accessible.rb
@@ -0,0 +1,12 @@
+module Kernel
+  def attr_accessible(*args)
+    args.each do |name|
+      define_method(name) do
+        instance_variable_get("@#{name}")
+      end
+      define_method("@#{name}=") do |new_value|
+        instance_variable_set("@#{name}=", new_value)
+      end
+    end
+  end
+end
