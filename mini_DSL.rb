b11c9c8b1bfea640ceac1a5e492277fbfed219c9
LeoXugu <robinleoknauth@gmail.com>
2017-09-25 19:22:57 -0700

    dsl

--- mini_DSL.rb
+++ mini_DSL.rb
@@ -3,4 +3,15 @@ class HTML
     instance_eval(&block)
   end
 
+  def method_missing(name, *args)
+  end
+
+end
+
+
+html = HTML.new do
+  h1 "Headline"
+  h3 "Smaller headline"
+  text "Long stupid text. Bla bla bla. Bla bla bla. Bla bla bla. Bla bla bla."
+
 end
