f8502dc81740463255e9e0ce1a2cd75ae66cf182
LeoXugu <robinleoknauth@gmail.com>
2017-09-26 12:20:58 -0700

    dsl

--- mini_DSL.rb
+++ mini_DSL.rb
@@ -4,6 +4,8 @@ class HTML
     instance_eval(&block)
   end
 
+  undef p
+
   def method_missing(name, *args)
     @rendered_html << "<#{name}><#{args[0]}></#{name}>"
   end
@@ -18,8 +20,8 @@ end
 html = HTML.new do
   h1 "Headline"
   h3 "Smaller headline"
-  text "Long stupid text. Bla bla bla. Bla bla bla. Bla bla bla. Bla bla bla."
-
+  p "Long stupid text. Bla bla bla. Bla bla bla. Bla bla bla. Bla bla bla."
+  
 end
 
 puts html.render
